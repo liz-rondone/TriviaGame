e15a9812fdc2c723e2f82353e0a738661586ff5d
liz-rondone <liz.rondone@northwestern.edu>
2017-06-07 22:21:57 -0500

    post tutor

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,36 @@
 // VARIABLES
 var count=60;
-var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
-var questions = [
-        {
-          question: "How many developers to screw in light bulb?",
-          answer: ["0", "1", "3", "4"],
-          correctAnswer: "0"
-        },
-        {
-          question: "What is the best band of all time?",
-          answer: ["Beatles", "New Order", "ABBA", "Carley Rae Jepson"],
-          correctAnswer: "ABBA"        
-        },
-        {
-          question: "What is the best band of all time?",
-          answer: ["Beatles", "New Order", "ABBA", "Carley Rae Jepson"],
-          correctAnswer: "ABBA"        
-        },
-        {
-          question: "What is the best band of all time?",
-          answer: ["Beatles", "New Order", "ABBA", "Carley Rae Jepson"],
-          correctAnswer: "ABBA"        
-        },
-        {
-          question: "What is the best band of all time?",
-          answer: ["Beatles", "New Order", "ABBA", "Carley Rae Jepson"],
-          correctAnswer: "ABBA"        
-        },
-      ];
+var counter;
+var questions = [{
+	question: "What does TBT stand for?",
+	answer: ["That Basic Tinder", "Throw Back Thursday", "Take Back Tuesday", "Ten Bad Turkeys"],
+	correctAnswer: "Throw Back Thursday"
+},
+{
+	question: "Someone who is “basic” is defined by enjoying which of the following?",
+	answer: ["Pumpkin Lattes", " UGG Boots", "Brunch", "All of the Above"],
+	correctAnswer: "All of the Above"        
+},
+{
+	question: "What is the best band of all time?",
+	answer: ["Beatles", "New Order", "ABBA", "Carley Rae Jepson"],
+	correctAnswer: "ABBA"        
+},
+{
+	question: "What is the best band of all time?",
+	answer: ["Beatles", "New Order", "ABBA", "Carley Rae Jepson"],
+	correctAnswer: "ABBA"        
+},
+{
+	question: "What is the best band of all time?",
+	answer: ["Beatles", "New Order", "ABBA", "Carley Rae Jepson"],
+	correctAnswer: "ABBA"        
+},];
 
 // EVENTS
 $('#start').on('click', function() {
-          startGame();
-        })
+	startGame();
+})
 
 
 // FUNCTIONS
@@ -42,49 +40,70 @@ function startGame() {
 	var questionString = '';
 	for(var i=0; i < questions.length; i++) {
 		j++;
-
 		var answerString = '';
         //classes for question div
-		questionString = '<div class="text-success">' + j + '. ' + questions[i].question + '</div>';
+		questionString = '<div class="text-success"><h4>' + j + '. ' + questions[i].question + '</h4></div>';
 		//classes for answer div
 		for(var a=0; a < questions[i].answer.length; a++) {
-			answerString += '<input class="answer" type="radio">' + questions[i].answer[a] + '</input>';
+			answerString += '<div class="answer"><input type="radio">' + questions[i].answer[a] + '</input></div>';
 		}
-		//classes for wrapper of BOTH question and answer
-		$('#questions').append('<div class="question">' + questionString + answerString + '</div>');
+
+		var questionDiv = $('<div></div>')
+		questionDiv.addClass('question')
+		questionDiv.attr('questionNum', i.toString())	//adding value and converting string to number
+		questionDiv.append(questionString)
+		questionDiv.append(answerString)
+		$('#questions').append(questionDiv)
+		//$('#questions').append('<div class="question">' + questionString + answerString + '</div>');
         
+        var submitter = '';
+        submitter = $('<div class="submitter"><input class="quizSubmit btn btn-info" id="submitButton" onClick="submitQuiz()" type="submit" value="Submit" /></div>')
+		$('#submitButton').append(submitter)
 	}
 
 }
 
 $('#start').click(function() {
-		$(this).addClass('hide');
-	});
+	counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+	$(this).addClass('hide');
+});
 
 // Timer
-function timer()
-{
-  count=count-1;
-  if (count <= 0)
-  {
-	console.log("Time's Up!");
-	clearInterval(counter);
-	updateTimer();
-	submitQuiz();
-	return;
+function timer() {
+	count=count-1;
+	if (count <= 0) {
+		console.log("Time's Up!");
+		clearInterval(counter);
+		updateTimer();
+		checkAnswer();
+		return;
 	}
-
 	updateTimer();
 }
 
 
 function updateTimer() {
-	document.getElementById("timer").innerHTML=count + " seconds";
+	//document.getElementById("timer").innerHTML=count + " seconds";
+	$('#timer').html(count + ' seconds');
 }
 
 
+function checkAnswer() {
+	console.log('submitted');
+
+	var correct;
+
+	for (var i = 0; i < questions.length; i++) {
+		if (radio[i].checked === questions.correctAnswer) {
+			correct++;
+			console.log(correct);
+		}
+	}
+	$('#submitButton').click('You answered ' correct ' out of 5 correctly.');
+}
+
 /*
-function submitQuiz() {
+function checkAnswer() {
 	console.log('submitted');
 	
 	// get each answer score
@@ -146,6 +165,11 @@ function submitQuiz() {
 	for (var i = 0, length = questionCountArray.length; i < length; i++) {
 		questionCounter++;
 	}
+
+if (calcScore === questionCounter) {
+	showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
+};
+document.getElementById('userScore').innerHTML = showScore;
 
 	// show score as "score/possible score"
 	var correctScore = "Answered Correctly: " + scoreGuessedRight; 		//  +"/" + questionCounter
