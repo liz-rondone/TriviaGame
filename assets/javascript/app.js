d756e20a1229ec967206ba0ca8795a2f518c714d
liz-rondone <liz.rondone@northwestern.edu>
2017-06-09 22:11:43 -0500

    friday add three

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,11 @@ var questions = [
 {
 	question: "What does TBT stand for?",
 	answer: ["Nothing, it’s not an acronym", "Throwback Thursday", "Take Back Tuesday", "Ten Bad Turkeys"],
-	correctAnswer: "Throw Back Thursday"
+	correctAnswer: "Throwback Thursday"
 },
 {
 	question: 'Someone who is “basic” is defined by enjoying which of the following?',
-	answer: ["Pumpkin lattes", "UGG boots", "Brunch", "All of the above"],
+	answer: ["Pumpkin lattes", "Lulu Lemon yoga pants", "Brunch", "All of the above"],
 	correctAnswer: "All of the above"        
 },
 {
@@ -63,8 +63,27 @@ $('#submitButton').click(function() {
 
 
 function results() {
-	$('#right').html(right + '/5 correct');
-	$('#wrong').html( wrong + '/5 wrong');
+	$('#right').html(right + '/5 Correct');
+	$('#wrong').html(wrong + '/5 Wrong');
+
+	if (right === 5) {
+		$('#results').html("For the win! You're Millennial AF.");
+	}
+	else if (right === 4) {
+		$('#results').html("Yaaaaaaas! #blessed");
+	}
+	else if (right === 3) {
+		$('#results').html("You're on fleek. Essentially, you did well but do you really want to be something only a Kardashian will use?");
+	}
+	else if (right === 2) {
+		$('#results').html("SMH \u2014 you could have tried harder.");
+	}
+	else if (right === 1) {
+		$('#results').html("The struggle is real.");
+	}
+	else if (right === 0) {
+		$('#results').html("You're so fetch. Stop trying to make fetch happen! It's not going to happen!");
+	}
 }
 
 
@@ -113,110 +132,3 @@ function updateTimer() {
 	//document.getElementById("timer").innerHTML=count + " seconds";
 	$('#timer').html(count + ' seconds');
 }
-
-
-/*function checkAnswer() {
-	console.log('submitted');
-
-	var correct;
-
-	for (var i = 0; i < questions.length; i++) {
-		if (radio[i].checked === questions.correctAnswer) {
-			correct++;
-			console.log(correct);
-		}
-	}
-	$('#results').click('You answered ' correct ' out of 5 correctly.');
-}
-*/
-/*
-function checkAnswer() {
-	console.log('submitted');
-	
-	// get each answer score
-	function answerScore (qName) {
-		var radiosNo = document.getElementsByName(qName);
-
-		for (var i = 0, length = radiosNo.length; i < length; i++) {
-   			if (radiosNo[i].checked) {
-				// do something with radiosNo
-				var answerValue = Number(radiosNo[i].value);
-			}
-		}
-		// change NaNs to zero
-		if (isNaN(answerValue)) {
-			answerValue = 0;
-		}
-		return answerValue;
-	}
-
-	// calc score with answerScore function
-	var scoreGuessedRight = (answerScore('questionOne') + answerScore('questionTwo') + answerScore('questionThree') + answerScore('questionFour') + answerScore('questionFive'));
-	console.log("scoreGuessedRight: " + scoreGuessedRight);
-
-	// calc score with answerScore function
-	var scoreGuessedWrong = $('.questions').length - scoreGuessedRight;
-	console.log("scoreGuessedWrong: " + scoreGuessedWrong);
-
-
-
-	// function to return correct answer string
-	function correctAnswer (correctStringNo, qNumber) {
-		console.log("qNumber: " + qNumber);  // logs 1,2,3,4,5 after called below
-		return ("The correct answer for question " + qNumber + ": &nbsp;<strong>" +
-		(document.getElementById(correctStringNo).innerHTML) + "</strong>");
-	}
-
-	// print correct answers only if wrong (calls correctAnswer function)
-	if (answerScore('questionOne') === 0) {
-		document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
-		console.log(correctAnswer('correctString1', 1));
-	}
-	if (answerScore('questionTwo') === 0) {
-		document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
-	}
-	if (answerScore('questionThree') === 0) {
-		document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
-	}
-	if (answerScore('questionFour') === 0) {
-		document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
-	}
-	if (answerScore('questionFive') === 0) {
-		document.getElementById('correctAnswer5').innerHTML = correctAnswer('correctString5', 5);
-	}
-
-	// calculate "possible score" integer
-	var questionCountArray = document.getElementsByClassName('question');
-
-	var questionCounter = 0;
-	for (var i = 0, length = questionCountArray.length; i < length; i++) {
-		questionCounter++;
-	}
-
-if (calcScore === questionCounter) {
-	showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
-};
-document.getElementById('userScore').innerHTML = showScore;
-
-	// show score as "score/possible score"
-	var correctScore = "Answered Correctly: " + scoreGuessedRight; 		//  +"/" + questionCounter
-	
-	document.getElementById('userCorrectScore').innerHTML = correctScore;
-
-	var incorrectScore = "Answered Incorrectly: " + scoreGuessedWrong; 		//  +"/" + questionCounter
-	document.getElementById('userIncorrectScore').innerHTML = incorrectScore;
-
-	//var incorrectScore = "Answered Inorrectly: " + scoreGuessedWrong; 		//  +"/" + questionCounter
-	//document.getElementById('userIncorrectScore').innerHTML = incorrectScore;
-}
-
-
-
-$(document).ready(function() {
-
-	$('#submitButton').click(function() {
-		$(this).addClass('hide');
-	});
-
-});
-*/
